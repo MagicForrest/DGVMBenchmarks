e1cf6462ee905980252ea340efc01cfc6a77ce8c
MargotK25 <margot_knapen@hotmail.com>
2022-12-09 16:07:52 +0100

    updated code for ICOS format

--- R/Format-ICOS.R
+++ R/Format-ICOS.R
@@ -1,147 +1,125 @@
 #!/usr/bin/Rscript
 
-#######################################################################################################################
-############################ FUNCTIONS TO HANDLE ICOS FILES ###########################################################
-#######################################################################################################################
+#####################################################################################################
+############################ FUNCTIONS TO HANDLE ICOS FILES #########################################
+#####################################################################################################
 
-#' Get a Field for LPJ-GUESS
+#' Get a Field for ICOS
 #' 
-#' An internal function that reads data from an LPJ-GUESS run.  
-#' It actually calls one of three other functions depending on the type of quantity specified.   
+#' An internal function that reads data from ICOS observations.   
 #' 
-#' @param source A \code{\linkS4class{Source}} containing the meta-data about the LPJ-GUESS run
-#' @param quant A string the define what output file from the LPJ-GUESS run to open, for example "anpp" opens and read the "anpp.out" file 
-#' @param layers A character string (or a vector of character strings) specifying which layer columns are to be read.  NULL (default) means read all.
-#' @param target.STAInfo The spatial-temporal target domain
-#' @param file.name Character string holding the name of the file.  This can be left blank, in which case the file name is just taken to be 
-#' "<quant@id>.out" (also "<quant@id>.out.gz")
+#' @param source A  \code{\linkS4class{Source}} containing the meta-data about the ICOS observations
+#' @param quant A Quantity object to define what quantity from the ICOS observations to extract
+#' @param layers Ignored for ICOS
+#' @param path.ICOS Path to where the ICOS files are stored
+#' @param UT.threshold USTAR threshold, variable (VUT) or constant (CUT). VUT is default
+#' @param partition.method Partition method, e.g. REF, USTAR50, MEAN. REF is default
+#' @param first.year Optional, will exclude data before this year
+#' @param last.year Optional, will exclude data beyond this year
 #' @param verbose A logical, set to true to give progress/debug information
 #' @return A list containing firstly the data.table containing the data, and secondly the STA.info 
-#' @author Matthew Forrest \email{matthew.forrest@@senckenberg.de}
+#' @author Margot Knapen \email{margot.knapen@@nateko.lu.se}
 #' @keywords internal
-#' 
-#' 
-#' 
-# ****************************** CREATE ICOS FORMAT ************************
-
-
-
-# extracting the quantities that are present in the observation dataset
-availableQuantities_obs <- function() {
-  
-  return(ICOS.quantities)
-  
-}
-
-# gridcells <-
-#   data.table(
-#     lon = lapply(sites_input, function(x) {
-#       slot(x, "longitude")
-#     }),
-#     lat = lapply(sites_input, function(x) {
-#       slot(x, "latitude")
-#     })
-#   )
-# 
-# gridcells[] <- lapply(gridcells, as.numeric)
+#'
 
 getField_ICOS <- function(source,
                           quant,
-                          layers,
-                          sta.info,
-                          file.name,
-                          verbose = FALSE,
-                          ...) {
+                          layers = NULL,
+                          path.ICOS,
+                          UT.threshold = "VUT",
+                          partition.method = "REF",
+                          first.year = "",
+                          last.year = "",
+                          verbose = F) {
+  
+  # variables that are currently supported
+  variables.cfluxes = c("GPP", "NEE", "Reco")
+  
+  # lists all files with daily fluxes
+  daily.files <- list.files(path = path.ICOS) %>%
+    stringr::str_subset("DD") %>% stringr::str_subset("VARINFO", negate = T)
+  
+  # list all files with site information
+  siteinfo.files <- list.files(path = path.ICOS) %>% stringr::str_subset("SITEINFO")
   
   
-  # read each individual ICOS text files, and compile everything into one big table
-  
-  
-  # determine which sites are available
-  
-  
-  # loop over each side 
-  for (s in 1:length(sites_input)) {
+  # loops through all available files with daily fluxes
+  for (i in 1:length(daily.files)) {
+    # reading the daily data from the .csv file
+    site.data <- read.csv(file = file.path(path.ICOS, daily.files[i]),
+                          na = c("-9999", "NA"))
     
-    # read the table
+    # reading the site info from the .csv file
+    siteinfo.data <- read.csv(file = file.path(path.ICOS, siteinfo.files[i]))
     
-    # select the columns that we want
+    # determining the site name abbreviation
+    site <- unlist(stringr::str_extract_all(string = daily.files[i],
+                                            pattern = "(?<=ICOSETC_).+(?=_FLUXNET)"))
     
-    # preselect years 
+    # determining the longitude and latitude
+    lon <- siteinfo.data$DATAVALUE[siteinfo.data$VARIABLE == "LOCATION_LONG"]
+    lat <- siteinfo.data$DATAVALUE[siteinfo.data$VARIABLE == "LOCATION_LAT"]
     
-    # look up lon and lat
+    # determining the full site name
+    site.name <- siteinfo.data$DATAVALUE[siteinfo.data$VARIABLE == "SITE_NAME"]
     
-    # add to list data.tables
     
-    # sets some commonly used input
-    variables_fluxes <-
-      trimws(strsplit(sites_input[[s]]@variables@variables_fluxes, ",")[[1]])
+    # declaring a new data table with metadata that will be used to append the daily fluxes to
+    site.data.selected <- data.table(Site = site,
+                                     Year = stringr::str_sub(site.data$TIMESTAMP, 1, 4),
+                                     Day = stringr::str_sub(site.data$TIMESTAMP, 7, 8),
+                                     ymd = site.data$TIMESTAMP,
+                                     Lon = lon,
+                                     Lat = lat)
     
-    # read the data from .csv file
-    obs_data <-
-      read.csv(
-        file = paste0(
-          sites_input[[s]]@paths@path_ICOS,
-          "/",
-          list.files(
-            path = sites_input[[s]]@paths@path_ICOS,
-            pattern = names(sites_input[s])
-          ) %>% str_subset("DD") %>%
-            str_subset("VARINFO", negate = T)
-        ),
-        na = c("-9999", "NA")
-      )
     
-    # declare a new data table to which the selected obs data will be added
-    obs_tbl <- data.table(
-      Site = names(sites_input[s]),
-      Year = str_sub(obs_data$TIMESTAMP, 1, 4),
-      Day = str_sub(obs_data$TIMESTAMP, 7, 8),
-      ymd = obs_data$TIMESTAMP,
-      Lon = sites_input[[s]]@longitude,
-      Lat = sites_input[[s]]@latitude
-    )
-    
-    # selects obs data corresponding to the flux variable
-    # adds day and night to get daily
+    # selecting the required columns (GPP, NEE, Reco) of the daily fluxes file
+    # adds day and night to get daily values
     # divides by a 1000 to convert gC/m^2 to kgC/m^2
-    for (v in variables_fluxes) {
-      obs_select <-
-        select(obs_data, contains(sites_input[[s]]@variables@UT_method)) %>%
+    for (v in variables.cfluxes) {
+      to.cbind <- select(site.data, contains(UT.threshold)) %>%
         select(contains(v)) %>%
-        select(contains(sites_input[[s]]@variables@suffix)) %>%
-        select(contains(c("_DT", "_NT")) |
-                 ends_with(c("DAY", "NIGHT"))) %>%
+        select(contains(partition.method)) %>%
+        select(contains(c("_DT", "_NT")) | ends_with(c("DAY", "NIGHT"))) %>%
         rowSums() / 1000
       
-      obs_tbl <- cbind(obs_tbl, obs_select)
-      setnames(obs_tbl, "obs_select", v)
-    }
-    
-    # selects data based on first/last year
-    # throws an error if no data is available for this time period
-    if (sites_input[[s]]@first_year != "") {
-      obs_tbl <-
-        obs_tbl[!(
-          as.numeric(obs_tbl$Year) < sites_input[[s]]@first_year |
-            as.numeric(obs_tbl$Year) > sites_input[[s]]@last_year
-        ),]
-      if (nrow(obs_tbl) == 0) {
-        stop("No data for the selected data seems to be available. Change the input file!")
-      }
+      site.data.selected <- cbind(site.data.selected, to.cbind)
+      setnames(site.data.selected, "to.cbind", v)
     }
     
     # convert day to day of year (doy)
-    obs_tbl$Day <- yday(ymd(obs_tbl$ymd))
-  
+    site.data.selected$Day <- lubridate::yday(lubridate::ymd(site.data.selected$ymd))
+    
+    if (length("first.year") > 0) {
+      site.data.selected <- site.data.selected[!(site.data.selected$Year) < first.year,]
+    } 
+    
+    if (length("last.year") > 0) {
+      site.data.selected <- site.data.selected[!(site.data.selected$Year) > last.year,]
+    }
+    
+    if (nrow(site.data.selected) == 0) {
+      warning("No data for this time period available. Check first and/or last year")
+      stop()
+    }
+    
+    # combine the data tables with selected daily fluxes for all sites
+    if (i == 1) {
+      ICOS.cfluxes <- site.data.selected
+    } else {
+      ICOS.cfluxes <- rbind(ICOS.cfluxes, site.data.selected)
+    }
   }
   
-  # combine list of data.tables
   
-  
-  if (quant@id %in% variables_fluxes) {
-    ICOS_cfluxes %>% select(Year, Day, Lon, Lat, quant@id) -> quant_data
+  # select the required columns for the current quantity
+  if (quant@id %in% variables.cfluxes) {
+    ICOS.cfluxes %>% select(Year, Day, Lon, Lat, quant@id) -> quant.data
   }
+  
+  # creating a data table with the lon/lat
+  gridcells <- data.table(Lat = unique(ICOS.cfluxes$Lat),
+                          Lon = unique(ICOS.cfluxes$Lon))
   
   field.id <-
     makeFieldID(
@@ -154,9 +132,9 @@ getField_ICOS <- function(source,
     "Field",
     id = field.id,
     quant = quant,
-    data = quant_data,
-    first.year =  min(as.numeric(quant_data$Year)),
-    last.year = max(as.numeric(quant_data$Year)),
+    data = quant.data,
+    first.year = first.year,
+    last.year = last.year,
     year.aggregate.method = "none",
     spatial.extent = gridcells,
     spatial.extent.id = "Gridcells",
@@ -168,51 +146,80 @@ getField_ICOS <- function(source,
   )
   
   return(return.field)
-  
 }
 
 
-#####################################################################
-########### LPJ-GUESS(-SPITFIRE) QUANTITIES ########################
-#####################################################################
 
+#####################################################################################################
+############################ ICOS AVAILABLE QUANTITIES ##############################################
+#####################################################################################################
+
+#' List ICOS quantities available
+#'
+#' Simply lists all carbon flux variables available in the ICOS data
+#' 
+#' @return A list of all ICOS quantities
+#' @keywords internal
+#'
+
+availableQuantities_ICOS <- function() {
+  
+  return(ICOS.quantities)
+  
+}
+
+#####################################################################################################
+############################ ICOS QUANTITIES ########################################################
+#####################################################################################################
 
 #' @format The \code{\linkS4class{Quantity}} class is an S4 class with the slots defined below
 #' @rdname Quantity-class
 #' @keywords datasets
 #' 
-#' 
+
 ICOS.quantities <- list(
   new("Quantity",
       id = "GPP",
       name = "GPP",
-      units = "gC/m^2",
+      units = "kgC/m^2",
       colours = function(n) rev(viridis::viridis(n)),
       format = c("ICOS"),
       standard_name = "gross_primary_productivity"),
   new("Quantity",
       id = "NEE",
       name = "NEE",
-      units = "gC/m^2",
+      units = "kgC/m^2",
       colours = function(n) rev(viridis::viridis(n)),
       format = c("ICOS"),
       standard_name = "net_ecosystem_exchange"),
   new("Quantity",
       id = "Reco",
       name = "Reco",
-      units = "gC/m^2",
+      units = "kgC/m^2",
       colours = function(n) rev(viridis::viridis(n)),
       format = c("ICOS"),
       standard_name = "ecosystem_respiration")
 )
 
 
-ICOS <- new(
-  "Format",
-  id = "ICOS",
-  predefined.layers = list(),
-  quantities = ICOS.quantities,
-  availableQuantities =
-    availableQuantities_obs,
-  getField = getField_ICOS
+
+#####################################################################################################
+############################ ICOS FORMAT ############################################################
+#####################################################################################################
+
+#' @description \code{ICOS} - a Format for reading ICOS observations
+#' 
+#' @format A \code{\linkS4class{Format}} object is an S4 class.
+#' @aliases Format-class
+#' @rdname Format-class
+#' @keywords datasets
+#' @export
+#' 
+
+ICOS <- new("Format",
+            id = "ICOS",
+            availableQuantities = availableQuantities_ICOS,
+            getField = getField_ICOS,
+            predefined.layers = list(),
+            quantities = ICOS.quantities
 )
