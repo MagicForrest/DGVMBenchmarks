74173e35f86af66c7b6231fac42b733c0dde098a
MargotK25 <margot_knapen@hotmail.com>
2023-05-11 12:36:21 +0200

    added option for data cleaning of FLUXNET2015 dataset, added option to remove leap days, changed Lon/Lat columns, bug fix

--- R/Format-FLUXNET.R
+++ R/Format-FLUXNET.R
@@ -20,9 +20,13 @@
 #' @param day.night.method Nighttime (NT) or daytime (DT) partition method for GPP and Reco. Default is NT
 #' @param first.year Optional, will exclude data before this year
 #' @param last.year Optional, will exclude data beyond this year
+#' @param rm.leap A logical, set to true to remove leap days
+#' @param data.cleaning A logical, set to true to apply data cleaning on FLUXNET2015 data
+#' @param qc.threshold Optional, set to a value between 0 and 1 below which data will be set to NA. Default is 0.5. Set to NULL to omit this from data cleaning 
 #' @import stringr
 #' @import dplyr
 #' @import lubridate
+#' @import readxl
 #' @return A list containing firstly the data.table containing the data, and secondly the STA.info 
 #' @author Margot Knapen \email{margot.knapen@@nateko.lu.se}
 #' @keywords internal
@@ -39,6 +43,9 @@ getField_FLUXNET <- function(source,
                               day.night.method = "NT",
                               first.year,
                               last.year,
+                              rm.leap = TRUE,
+                              data.cleaning = TRUE,
+                              qc.threshold = 0.5,
                               ...) {
   
   ### CHECK ARGUEMENTS
@@ -46,7 +53,6 @@ getField_FLUXNET <- function(source,
     if(first.year > last.year) stop("first.year cannot be greater than last.year!")
   }
 
-  
   # variables that are currently supported
   variables.cfluxes = c("GPP", "NEE", "Reco")
   
@@ -57,164 +63,240 @@ getField_FLUXNET <- function(source,
   
   # determine file type
   CH4.files <- daily.files[grep("CH4", daily.files)]
-  FLUXNET2015.files <- daily.files[-grep("CH4", daily.files)]
-  
-  # loop over all FLUXNET-CH4 files
-  for (i in 1:length(CH4.files)) {
-    # read the daily data from the .csv file
-    site.data <- read.csv(file = file.path(source@dir, CH4.files[i]), na = c("-9999", "NA"))
-    
-    # read the site info from the .csv file
-    siteinfo.files <- list.files(path = source@dir) %>% stringr::str_subset("AA-Flx") %>%
-      stringr::str_subset("CH4")
-    siteinfo.data <- read.csv(file = file.path(source@dir, siteinfo.files))
-    
-    # determine site code
-    site <- unlist(stringr::str_extract_all(string = CH4.files[i],
-                                            pattern = "(?<=FLX_).+(?=_FLUXNET)"))
-    
-    # determine the longitude and latitude
-    lon <- as.numeric(as.character(siteinfo.data$LON[siteinfo.data$SITE_ID == site]))
-    lat <- as.numeric(as.character(siteinfo.data$LAT[siteinfo.data$SITE_ID == site]))
-    
-    # determine the full site name
-    site.name <- siteinfo.data$SITE_NAME[siteinfo.data$SITE_ID == site]
-    
-    # declaring a new data table with metadata that will be used to append the daily fluxes to
-    site.data.selected <- data.table(Code = site,
-                                     Name = site.name,
-                                     Year = as.integer(stringr::str_sub(site.data$TIMESTAMP, 1, 4)),
-                                     Day = as.integer(stringr::str_sub(site.data$TIMESTAMP, 7, 8)),
-                                     ymd = site.data$TIMESTAMP,
-                                     Lon = as.numeric(lon),
-                                     Lat = as.numeric(lat))
-    
-    # selecting the required columns (GPP, NEE, Reco) of the daily fluxes file
-    # adds day and night to get daily values
-    # divides by a 1000 to convert gC/m^2 to kgC/m^2
-    for (v in variables.cfluxes) {
-      to.cbind <- select(site.data, contains(toupper(v))) %>%
-        select(contains(day.night.method) | ends_with("_F")) %>%
-        rowSums() / 1000
-      
-      site.data.selected <- cbind(site.data.selected, to.cbind)
-      setnames(site.data.selected, "to.cbind", v)
-    }
-   
-    # convert day to day of year (doy)
-    site.data.selected$Day <- as.integer(lubridate::yday(lubridate::ymd(site.data.selected$ymd)))
-    
-    if (!missing(first.year)) {
-      site.data.selected <- site.data.selected[!(site.data.selected$Year) < first.year,]
-    } 
-    
-    if (!missing(last.year)) {
-      site.data.selected <- site.data.selected[!(site.data.selected$Year) > last.year,]
-    } 
-
-    if (nrow(site.data.selected) == 0) {
-      warning("No data for this time period available. Check first and/or last year")
-      stop()
-    }
-    
-    # combine the data tables with selected daily fluxes for all sites
-    if (i == 1) {
-      FLUXNETCH4.cfluxes <- site.data.selected
-    } else {
-      FLUXNETCH4.cfluxes <- rbind(FLUXNETCH4.cfluxes, site.data.selected)
-    }
-  }
-  
-  
-  # loop over all FLUXNET2015 files
-  for (i in 1:length(FLUXNET2015.files)) {
-    # read the daily data from the .csv file
-    site.data <- read.csv(file = file.path(source@dir, FLUXNET2015.files[i]), na = c("-9999", "NA"))
-
-    # read the site info from the .csv file
-    siteinfo.files <- list.files(path = source@dir) %>% stringr::str_subset("AA-Flx") %>%
-      stringr::str_subset("DD")
-    siteinfo.data <- read_xlsx(path = file.path(source@dir, siteinfo.files))
-    
-    # determine site code
-    site <- unlist(stringr::str_extract_all(string = FLUXNET2015.files[i],
-                                            pattern = "(?<=FLX_).+(?=_FLUXNET)"))
-    
-    # determine the longitude and latitude
-    lon <- as.numeric(as.character(siteinfo.data$DATAVALUE[siteinfo.data$VARIABLE == "LOCATION_LONG" &
-                                                             siteinfo.data$SITE_ID == site]))
-    lat <- as.numeric(as.character(siteinfo.data$DATAVALUE[siteinfo.data$VARIABLE == "LOCATION_LAT" &
-                                                             siteinfo.data$SITE_ID == site]))
-    
-    # determine the full site name
-    site.name <- siteinfo.data$DATAVALUE[siteinfo.data$VARIABLE == "SITE_NAME" &
-                                           siteinfo.data$SITE_ID == site]
-    
-    # declaring a new data table with metadata that will be used to append the daily fluxes to
-    site.data.selected <- data.table(Code = site,
-                                     Name = site.name,
-                                     Year = as.integer(stringr::str_sub(site.data$TIMESTAMP, 1, 4)),
-                                     Day = as.integer(stringr::str_sub(site.data$TIMESTAMP, 7, 8)),
-                                     ymd = site.data$TIMESTAMP,
-                                     Lon = as.numeric(lon),
-                                     Lat = as.numeric(lat))
-    
-    # selecting the required columns (GPP, NEE, Reco) of the daily fluxes file
-    # adds day and night to get daily values
-    # divides by a 1000 to convert gC/m^2 to kgC/m^2
-    for (v in variables.cfluxes) {
-      to.cbind <- select(site.data, contains(UT.threshold)) %>%
-        select(contains(v)) %>%
-        select(contains(partition.method)) %>%
-        select(contains(day.night.method) | ends_with(partition.method)) %>%
-        rowSums() / 1000
-      
-      site.data.selected <- cbind(site.data.selected, to.cbind)
-      setnames(site.data.selected, "to.cbind", v)
-    }
-    
-    # convert day to day of year (doy)
-    site.data.selected$Day <- as.integer(lubridate::yday(lubridate::ymd(site.data.selected$ymd)))
-    
-    if (!missing(first.year)) {
-      site.data.selected <- site.data.selected[!(site.data.selected$Year) < first.year,]
-    } 
-    
-    if (!missing(last.year)) {
-      site.data.selected <- site.data.selected[!(site.data.selected$Year) > last.year,]
-    } 
-    
-    if (nrow(site.data.selected) == 0) {
-      warning("No data for this time period available. Check first and/or last year")
-      stop()
-    }
-    
-    # combine the data tables with selected daily fluxes for all sites
-    if (i == 1) {
-      FLUXNET2015.cfluxes <- site.data.selected
-    } else {
-      FLUXNET2015.cfluxes <- rbind(FLUXNET2015.cfluxes, site.data.selected)
-    }
-  }
-  
-  if (exists("FLUXNETCH4.cfluxes") == T & exists("FLUXNET2015.cfluxes") == T) {
-    FLUXNET.cfluxes <- rbind(FLUXNETCH4.cfluxes, FLUXNET2015.cfluxes)
-  } else if (exists("FLUXNETCH4.cfluxes") == T) {
-    FLUXNET.cfluxes <- FLUXNETCH4.cfluxes
+  if (length(CH4.files) == 0) {
+    FLUXNET2015.files <- daily.files
   } else {
-    FLUXNET.cfluxes <- FLUXNET2015.cfluxes
+    FLUXNET2015.files <- daily.files[-grep("CH4", daily.files)]
+  }
+  
+  if (length(CH4.files) != 0) {
+    
+    # loop over all FLUXNET-CH4 files
+    for (i in 1:length(CH4.files)) {
+      
+      # read the daily data from the .csv file
+      site.data <- read.csv(file = file.path(source@dir, CH4.files[i]), na = c("-9999", "NA"))
+      
+      # read the site info from the .csv file
+      siteinfo.files <- list.files(path = source@dir) %>% stringr::str_subset("AA-Flx") %>%
+        stringr::str_subset("CH4")
+      siteinfo.data <- read.csv(file = file.path(source@dir, siteinfo.files))
+      
+      # determine site code
+      site <- unlist(stringr::str_extract_all(string = CH4.files[i],
+                                              pattern = "(?<=FLX_).+(?=_FLUXNET)"))
+      
+      # determine the longitude and latitude
+      lon <- as.numeric(as.character(siteinfo.data$LON[siteinfo.data$SITE_ID == site]))
+      lat <- as.numeric(as.character(siteinfo.data$LAT[siteinfo.data$SITE_ID == site]))
+      
+      # determine the full site name
+      site.name <- siteinfo.data$SITE_NAME[siteinfo.data$SITE_ID == site]
+      
+      # declaring a new data table with metadata that will be used to append the daily fluxes to
+      site.data.selected <- data.table(Code = site,
+                                       Name = site.name,
+                                       Year = as.integer(stringr::str_sub(site.data$TIMESTAMP, 1, 4)),
+                                       Day = as.integer(stringr::str_sub(site.data$TIMESTAMP, 7, 8)),
+                                       ymd = site.data$TIMESTAMP,
+                                       Lon_FLUXNET = as.numeric(lon),
+                                       Lat_FLUXNET = as.numeric(lat),
+                                       Lon = round((as.numeric(lon) + 0.25) * 2) / 2 - 0.25,
+                                       Lat = round((as.numeric(lat) + 0.25) * 2) / 2 - 0.25)
+
+      # selecting the required columns (GPP, NEE, Reco) of the daily fluxes file
+      # umolCO2/m^2/s to kgC/m^2
+      to.cbind <- select(site.data, contains(quant@id)) %>%
+        select(contains(day.night.method) | ends_with("_F")) %>%
+        rowSums() * 1e-06 * 0.012011 * 86400 
+      
+      if (data.cleaning == TRUE) {
+        # set negative GPP / Reco values to NA
+        if (quant@name == "GPP" | quant@name == "Reco") {
+          to.cbind[which(to.cbind < 0)] <- NA
+        }
+      }
+      
+      site.data.selected <- cbind(site.data.selected, to.cbind)
+      setnames(site.data.selected, "to.cbind", quant@id)
+      
+      site.data.selected$Day <- as.integer(lubridate::yday(lubridate::ymd(site.data.selected$ymd)))
+      
+      # remove leap days and / or convert day to day
+      if (rm.leap == TRUE) {
+        indx <- which(site.data.selected$Day == 60 & leap_year(site.data.selected$Year))
+        if (length(indx) > 0) {
+          site.data.selected <- site.data.selected[-indx,]
+          
+          site.data.selected$Day[which(leap_year(site.data.selected$Year) &
+                                         site.data.selected$Day > 60)] <- site.data.selected$Day[which(leap_year(site.data.selected$Year) &
+                                                                                                         site.data.selected$Day > 60)]  - 1
+        }
+      }
+      
+      if (!missing(first.year)) {
+        site.data.selected <- site.data.selected[!(site.data.selected$Year < first.year),]
+      } 
+      
+      if (!missing(last.year)) {
+        site.data.selected <- site.data.selected[!(site.data.selected$Year > last.year),]
+      } 
+      
+      if (nrow(site.data.selected) == 0) {
+        warning("No data for this time period available. Check first and/or last year")
+        stop()
+      }
+      
+      # combine the data tables with selected daily fluxes for all sites
+      if (i == 1) {
+        FLUXNETCH4.cfluxes <- site.data.selected
+      } else {
+        FLUXNETCH4.cfluxes <- rbind(FLUXNETCH4.cfluxes, site.data.selected)
+      }
+    }
+  }
+  
+  if (length(FLUXNET2015.files) != 0) {
+    
+    # loop over all FLUXNET2015 files
+    for (i in 1:length(FLUXNET2015.files)) {
+      
+      # read the daily data from the .csv file
+      site.data <- read.csv(file = file.path(source@dir, FLUXNET2015.files[i]), na = c("-9999", "NA"))
+      
+      # read the site info from the .csv file
+      siteinfo.files <- list.files(path = source@dir) %>% stringr::str_subset("AA-Flx") %>%
+        stringr::str_subset("DD")
+      siteinfo.data <- readxl::read_xlsx(path = file.path(source@dir, siteinfo.files))
+      
+      # determine site code
+      site <- unlist(stringr::str_extract_all(string = FLUXNET2015.files[i],
+                                              pattern = "(?<=FLX_).+(?=_FLUXNET)"))
+      
+      # determine the longitude and latitude
+      lon <- as.numeric(as.character(siteinfo.data$DATAVALUE[siteinfo.data$VARIABLE == "LOCATION_LONG" &
+                                                               siteinfo.data$SITE_ID == site]))
+      lat <- as.numeric(as.character(siteinfo.data$DATAVALUE[siteinfo.data$VARIABLE == "LOCATION_LAT" &
+                                                               siteinfo.data$SITE_ID == site]))
+      
+      # determine the full site name
+      site.name <- siteinfo.data$DATAVALUE[siteinfo.data$VARIABLE == "SITE_NAME" &
+                                             siteinfo.data$SITE_ID == site]
+      
+      # declaring a new data table with metadata that will be used to append the daily fluxes to
+      site.data.selected <- data.table(Code = site,
+                                       Name = site.name,
+                                       Year = as.integer(stringr::str_sub(site.data$TIMESTAMP, 1, 4)),
+                                       Day = as.integer(stringr::str_sub(site.data$TIMESTAMP, 7, 8)),
+                                       ymd = site.data$TIMESTAMP,
+                                       Lon_FLUXNET = as.numeric(lon),
+                                       Lat_FLUXNET = as.numeric(lat),
+                                       Lon = round((as.numeric(lon) + 0.25) * 2) / 2 - 0.25,
+                                       Lat = round((as.numeric(lat) + 0.25) * 2) / 2 - 0.25)
+      
+      # selecting the required columns (GPP, NEE, Reco) of the daily fluxes file
+      # divides by a 1000 to convert gC/m^2 to kgC/m^2
+      if (quant@name == "NEE" & day.night.method == "NT") {
+        day.night.method <- "NIGHT"
+      } else if (quant@name == "NEE" & day.night.method == "DT") {
+        day.night.method <- "DAY"
+      }
+      
+      if (quant@name == "NEE") {
+        to.cbind <- select(site.data, contains(UT.threshold)) %>%
+          select(contains(quant@name)) %>%
+          select(contains(partition.method)) %>%
+          select(ends_with(day.night.method)) %>%
+          rowSums() / 1000
+        
+      } else {
+        to.cbind <- select(site.data, contains(UT.threshold)) %>%
+          select(contains(quant@name)) %>%
+          select(contains(partition.method)) %>%
+          select(contains(day.night.method)) %>%
+          rowSums() / 1000
+      }
+
+      # data cleaning
+      if (data.cleaning == TRUE) {
+        # set negative GPP / Reco values to NA
+        if (quant@name == "GPP" | quant@name == "Reco") {
+          to.cbind[which(to.cbind < 0)] <- NA
+        }
+
+        # set values with NEE QC below threshold as NA
+        if (is.null(qc.threshold) == FALSE) {
+          if (day.night.method == "NT" | day.night.method == "NIGHT") {
+            day.night.method.NEE <- "NIGHT"
+          } else if (day.night.method == "DT" | day.night.method == "DAY") {
+            day.night.method.NEE <- "DAY"
+          }
+          
+          nee.qc.data <- select(site.data, contains(UT.threshold)) %>%
+            select(contains("NEE")) %>% select(contains(partition.method)) %>%
+            select(contains(day.night.method.NEE)) %>% select(ends_with("QC"))
+          
+          to.cbind[which(nee.qc.data < qc.threshold)] <- NA
+        }
+        
+      }
+
+      site.data.selected <- cbind(site.data.selected, to.cbind)
+      setnames(site.data.selected, "to.cbind", quant@name)
+      
+      # remove leap days and / or convert day to day
+      if (rm.leap == TRUE) {
+        indx <- which(site.data.selected$Day == 60 & leap_year(site.data.selected$Year))
+        if (length(indx) > 0) {
+          site.data.selected <- site.data.selected[-indx,]
+          
+          site.data.selected$Day[which(leap_year(site.data.selected$Year) &
+                                         site.data.selected$Day > 60)] <- site.data.selected$Day[which(leap_year(site.data.selected$Year) &
+                                                                                                         site.data.selected$Day > 60)]  - 1
+        }
+      }
+      
+      if (!missing(first.year)) {
+        site.data.selected <- site.data.selected[!(site.data.selected$Year < first.year),]
+      } 
+      
+      if (!missing(last.year)) {
+        site.data.selected <- site.data.selected[!(site.data.selected$Year > last.year),]
+      } 
+      
+      if (nrow(site.data.selected) == 0) {
+        warning("No data for this time period available. Check first and/or last year")
+        stop()
+      }
+      
+      # combine the data tables with selected daily fluxes for all sites
+      if (i == 1) {
+        FLUXNET2015.cfluxes <- site.data.selected
+      } else {
+        FLUXNET2015.cfluxes <- rbind(FLUXNET2015.cfluxes, site.data.selected)
+      }
+    }
+    
+    if (exists("FLUXNETCH4.cfluxes") == T & exists("FLUXNET2015.cfluxes") == T) {
+      FLUXNET.cfluxes <- rbind(FLUXNETCH4.cfluxes, FLUXNET2015.cfluxes)
+    } else if (exists("FLUXNETCH4.cfluxes") == T) {
+      FLUXNET.cfluxes <- FLUXNETCH4.cfluxes
+    } else {
+      FLUXNET.cfluxes <- FLUXNET2015.cfluxes
+    }
   }
   
   
   # select the required columns for the current quantity
   if (quant@id %in% variables.cfluxes) {
-    FLUXNET.cfluxes %>% select(Year, Day, Lon, Lat, quant@id) -> quant.data
+    FLUXNET.cfluxes %>% select(Year, Day, Lon, Lat, Lon_FLUXNET, Lat_FLUXNET, quant@id) -> quant.data
   }
   
-  
   # creating a data table with the lon/lat
-  gridcells <- data.table(Lon = as.numeric(unique(FLUXNET.cfluxes$Lon)),
-                          Lat = as.numeric(unique(FLUXNET.cfluxes$Lat)),
+  gridcells <- data.table(Lon = as.numeric(round((unique(FLUXNET.cfluxes$Lon_FLUXNET) + 0.25) * 2) / 2 - 0.25),
+                          Lat = as.numeric(round((unique(FLUXNET.cfluxes$Lat_FLUXNET) + 0.25) * 2) / 2 - 0.25),
+                          Lon_FLUXNET = as.numeric(unique(FLUXNET.cfluxes$Lon_FLUXNET)),
+                          Lat_FLUXNET = as.numeric(unique(FLUXNET.cfluxes$Lat_FLUXNET)),
                           Code = unique(FLUXNET.cfluxes$Code),
                           Name = unique(FLUXNET.cfluxes$Name))
   field.id <-
