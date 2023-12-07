66eb76592abb41a02c62c0adfd144401667d3a53
Karl Piltz <Karl.Piltz@nateko.lu.se>
2023-12-07 16:12:15 +0100

    Function to check data source and process if present

---
+++ R/processDataSource.R
@@ -0,0 +1,48 @@
+#' Process the current dataset to be used in benchmark. 
+#' Checks if directory and file exists and then processes the dataset source to get field based on ins file.
+#'
+#' @param all_datasets List of available datasets. (Can be empty)
+#' @param input YAML input file
+#' @param benchmark_name name of current benchmark needed to direct input file
+#' @param simulation integer telling which simulation to use from the input list
+#' @param dataset can be changed if multiple datasets are used. Default is 1.
+#'
+#' @return Data.year.mean (fully processed dataset field)
+#' @export
+#'
+#' @examples Data.year.mean <- processDataSource(all_datasets, input, benchmark_name, simulation = 1)
+#' @author Karl Piltz (karl.piltz@@nateko.lu.se)
+processDataSource <- function(all_datasets, input, benchmark_name, simulation, dataset = 1) {
+  
+  if (length(all_datasets) == 0) {
+    Data.year.mean <- all_datasets
+    return(Data.year.mean)
+  } else{
+  
+  this_dataset_run_dir <- file.path(all_datasets[[dataset]]@dir, input[["Directory"]][["Simulation_name"]][[simulation]])
+  
+  if (dir.exists(this_dataset_run_dir) &&
+      (file.exists(file.path(this_dataset_run_dir, paste0(input[[benchmark_name]][["File_name"]], ".out"))) ||
+       file.exists(file.path(this_dataset_run_dir, paste0(input[[benchmark_name]][["File_name"]], ".out.gz"))))) {
+        
+        this_data_Source <- all_datasets[[dataset]]
+        this_data_Source@dir <- file.path(this_dataset_run_dir)
+        
+        Data.year.mean <- DGVMTools::getField(source = this_data_Source,
+                                              quant = input[[benchmark_name]][["File_name"]],
+                                              layers = input[[benchmark_name]][["Layer"]],
+                                              first.year = as.numeric(input[[benchmark_name]][["First_year"]]),
+                                              last.year = as.numeric(input[[benchmark_name]][["Last_year"]]),
+                                              spatial.extent.id = input[["Directory"]][["spatial_extent_id"]],
+                                              spatial.extent = spatial.extent,
+                                              year.aggregate.method = "mean")
+        
+        Data.year.mean@source@name <- input[[benchmark_name]][["Dataset_name"]]
+        return(Data.year.mean)
+        }
+      }
+}
+
+
+
+
