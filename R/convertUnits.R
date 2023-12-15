f06dcc8ea1589a6042c9f5c81904bb902436c537
Karl Piltz <Karl.Piltz@nateko.lu.se>
2023-12-15 17:43:32 +0100

    fix

--- R/convertUnits.R
+++ R/convertUnits.R
@@ -11,23 +11,25 @@
 #' @examples convertUnits(Data.year.mean, "indiv/ha", "indiv/km^2")
 convertUnits <- function(df, from, to, benchmark = this_benchmark) {
   
-  units <- list("indiv/ha","indiv/km^2","indiv/m^2","kgC/ha","kgC/km^2", "kgC/m^2","kgC/ha/y","kgC/km^2/y","kgC/m^2/year","gC/ha/y", "gC/km^2/y", "gC/m^2/year", "cm","m","mm","dm")
-  
+  units <- list("indiv/ha","indiv/km2","indiv/m2","kgC/ha","kgC/km2", "kgC/m2","kgC/ha/y","kgC/km2/y","kgC/m2/year","gC/ha/y", "gC/km2/y", "gC/m2/year", "cm","m","mm","dm")
+ 
   if (!(from %in% units)) {
+    print(units)
     stop("Invalid unit for from. Please provide valid units.")
   }
   if (!(to %in% units)) {
+    print(units)
     stop("Invalid unit for to. Please provide valid units.")
   }
   
-  if (from == "indiv/ha" && to == "indiv/km^2") {
+  
+  if (from == "indiv/ha" && to == "indiv/km2") {
     conversion_factor <- 0.0001
-  } else if (from == "indiv/m^2" && to == "indiv/ha") {
+  } else if (from == "indiv/m2" && to == "indiv/ha") {
     conversion_factor <- 10000
-  } else if (from == "indiv/m^2" && to == "indiv/km^2") {
+  } else if (from == "indiv/m2" && to == "indiv/km2") {
     conversion_factor <- 1000000
-  }
-  else if (from == "cm" & to == "m") {
+  } else if (from == "cm" & to == "m") {
     conversion_factor <- 0.01
   } else if (from == "cm" & to == "mm") {
     conversion_factor <- 10
@@ -51,23 +53,23 @@ convertUnits <- function(df, from, to, benchmark = this_benchmark) {
     conversion_factor <- 0.1
   } else if (from == "dm" & to == "mm") {
     conversion_factor <- 100
-  } else if (from == "kgC/ha" & to == "kgC/km^2") {
+  } else if (from == "kgC/ha" & to == "kgC/km2") {
     conversion_factor <- 0.0001
-  } else if (from == "kgC/m^2" & to == "kgC/ha") {
+  } else if (from == "kgC/m2" & to == "kgC/ha") {
     conversion_factor <- 10000
-  } else if (from == "kgC/m^2" & to == "kgC/km^2") {
+  } else if (from == "kgC/m2" & to == "kgC/km2") {
     conversion_factor <- 1e6
-  } else if (from == "kgC/ha/y" & to == "kgC/km^2/y") {
+  } else if (from == "kgC/ha/y" & to == "kgC/km2/y") {
     conversion_factor <- 0.0001
   } else if (from == "kgC/m^2/year" & to == "kgC/ha/y") {
     conversion_factor <- 10000
-  } else if (from == "kgC/m^2/year" & to == "kgC/km^2/y") {
+  } else if (from == "kgC/m2/year" & to == "kgC/km2/y") {
     conversion_factor <- 1e6
   } else if (from == "gC/ha/y" & to == "kgC/ha/y") {
     conversion_factor <- 0.001
-  } else if (from == "gC/km^2/y" & to == "kgC/km^2/y") {
+  } else if (from == "gC/km2/y" & to == "kgC/km2/y") {
     conversion_factor <- 1e6
-  } else if (from == "gC/m^2/year" & to == "kgC/m^2/year") {
+  } else if (from == "gC/m2/year" & to == "kgC/m2/year") {
     conversion_factor <- 0.001
   } else {stop("Invalid conversion. Please provide valid conversion.")}
   
