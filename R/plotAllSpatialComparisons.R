228afd8662e884dc134be345f50bf2097efae9df
Karl Piltz <Karl.Piltz@nateko.lu.se>
2024-02-16 16:28:41 +0100

    Added data spatial mean when data is present in comparison.

--- R/plotAllSpatialComparisons.R
+++ R/plotAllSpatialComparisons.R
@@ -14,7 +14,7 @@ plotAllSpatialComparisons <- function(Benchmark, all_comparisons) {
   for (i in seq_along(all_comparisons[["Values"]])) {
     this_comparison <- all_comparisons[["Values"]][[i]]
 
-
+  if (this_comparison@source2@id == "Obs"){
     #Quick plot of comparison object
     DGVMTools::plotSpatialComparison(this_comparison, type = "difference")
 
@@ -22,7 +22,7 @@ plotAllSpatialComparisons <- function(Benchmark, all_comparisons) {
     max_dif<-round(max(this_comparison@data[[5]]),2)
     mean_dif<-round(mean(this_comparison@data[[5]]),2)
     min_dif<-round(min(this_comparison@data[[5]]),2)
-
+    mean_obs<-round(mean(this_comparison@data[[4]]),2)
     # Plotting difference maps
     p1 <- DGVMTools::plotSpatialComparison(this_comparison, type = "difference", map.overlay = "world", panel.bg.col = "gray")+
       scale_fill_gradient2(low = "red", high = "blue", mid = "white",
@@ -32,10 +32,35 @@ plotAllSpatialComparisons <- function(Benchmark, all_comparisons) {
                            breaks = unlist(Benchmark@breaks))+
       labs(title = paste(this_comparison@name), subtitle = paste("Max:", max_dif,
                                                                  "Mean:", mean_dif,
-                                                                 "Min:", min_dif))+
+                                                                 "Min:", min_dif,
+                                                                 "Data Mean:", mean_obs))+
       theme(plot.title = element_text(size = 30), plot.subtitle = element_text(size = 20), axis.title.x = element_text(size = 25),
             axis.title.y = element_text(size = 25),legend.text = element_text(size = 17),legend.title = element_text(size = 19), panel.border = element_rect(color = "black", fill = NA, size = 1))
     plot(p1)
+  }
+    else{
+      DGVMTools::plotSpatialComparison(this_comparison, type = "difference")
+      
+      #Get the Max, Min, Mean difference of Model - Observed data
+      max_dif<-round(max(this_comparison@data[[5]]),2)
+      mean_dif<-round(mean(this_comparison@data[[5]]),2)
+      min_dif<-round(min(this_comparison@data[[5]]),2)
+      
+      # Plotting difference maps
+      p1 <- DGVMTools::plotSpatialComparison(this_comparison, type = "difference", map.overlay = "world", panel.bg.col = "gray")+
+        scale_fill_gradient2(low = "red", high = "blue", mid = "white",
+                             midpoint = 0,
+                             na.value = "black",
+                             limits = unlist(Benchmark@limits),
+                             breaks = unlist(Benchmark@breaks))+
+        labs(title = paste(this_comparison@name), subtitle = paste("Max:", max_dif,
+                                                                   "Mean:", mean_dif,
+                                                                   "Min:", min_dif
+                                                                   ))+
+        theme(plot.title = element_text(size = 30), plot.subtitle = element_text(size = 20), axis.title.x = element_text(size = 25),
+              axis.title.y = element_text(size = 25),legend.text = element_text(size = 17),legend.title = element_text(size = 19), panel.border = element_rect(color = "black", fill = NA, size = 1))
+      plot(p1)
+    }
   }
   }
 
