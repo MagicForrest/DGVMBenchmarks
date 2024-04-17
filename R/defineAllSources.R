5a2b0ce64c99e560057ecfd921bcfea225c3b46b
Karl Piltz <Karl.Piltz@nateko.lu.se>
2024-04-17 13:29:01 +0200

    New way to handle Formats per benchmark

--- R/defineAllSources.R
+++ R/defineAllSources.R
@@ -1,28 +1,61 @@
 
 #' Define all sources
 #'
-#' @param input yaml instruction file
-#' @param format Holding format
+#' @param input yaml instruction file holding Benchmark Formats
+#' 
 #'
 #' @return List of lists for the dataset source and the simulations sources
 #' @export
 #'
-#' @examples defineAllSources(input = input_data, format = format)
+#' @examples defineAllSources(input = input_data)
 #' @author Karl Piltz (karl.piltz@@nateko.lu.se)
-defineAllSources <- function(input, format){
-  ## Set Model and reference sources ##
+defineAllSources <- function(input){
+  all_formats <- c()
+  for (i in 3:length(input)) {
+    # Extract Data$Format from the current object and add it to the all_formats vector
+    all_formats <- c(all_formats, input[[i]]$Data$Format)
+    # Extract Simulations$Format from the current object and add it to the all_formats vector
+    all_formats <- c(all_formats, input[[i]]$Simulations$Format)
+  }
+  
+  # Get all Formats to be processed
+  Formats <- unique(all_formats)
   all_datasets <- list()
-  if (is.na(input[["Directory"]][["Data"]]) || input[["Directory"]][["Data"]] == ""){input[["Directory"]][["Data"]] <- NULL}
-  if (!is.null(input[["Directory"]][["Data"]])){
-  Reference_data <- DGVMTools::defineSource(id = "Obs", dir = input[["Directory"]][["Data"]], format = format, name = "Obs")
-  all_datasets <- list(Reference_data)}
-
-  # Define source for latest model run
-  New_run <- DGVMTools::defineSource(id = input[["Directory"]][["New_id"]], dir = input[["Directory"]][["New"]], format = format, name = input[["Directory"]][["New_id"]])
-  # If previous model run is to be included, defined here. All sources added to list for benchmarking.
-  if (is.na(input[["Directory"]][["Old"]]) || input[["Directory"]][["Old"]] == ""){input[["Directory"]][["Old"]] <- NULL}
-  if (!is.null(input[["Directory"]][["Old"]])){Old_run <- DGVMTools::defineSource(id = input[["Directory"]][["Old_id"]], dir = input[["Directory"]][["Old"]], format = format, name = input[["Directory"]][["Old_id"]])
-  all_simulation_Sources_list <- list(New_run, Old_run)
-  }else{all_simulation_Sources_list <- list(New_run)}
+  all_simulation_Sources_list <- list()
+  ## Set Model and reference sources ##
+  all_GUESS_simulation_Sources_list <- list()
+  if ("GUESS" %in% Formats){
+    GUESS_sources <- DGVMBenchmarks::define_GUESS_Sources(input = input)
+    all_GUESS_datasets <- GUESS_sources[[1]]
+    all_datasets[["GUESS"]] <- all_GUESS_datasets
+    all_GUESS_simulation_Sources_list <- GUESS_sources[[2]]
+    all_simulation_Sources_list[["GUESS"]] <- all_GUESS_simulation_Sources_list
+  }
+  
+  all_NetCDF_simulation_Sources_list <- list()
+  if ("NetCDF" %in% Formats){
+    NetCDF_sources <- DGVMBenchmarks::define_NetCDF_Sources(input = input)
+    all_NetCDF_datasets <- NetCDF_sources[[1]]
+    all_datasets[["NetCDF"]] <- all_NetCDF_datasets
+    all_NetCDF_simulation_Sources_list <- NetCDF_sources[[2]]
+    all_simulation_Sources_list[["NetCDF"]] <- all_NetCDF_simulation_Sources_list}
+  
+  all_SITE_simulation_Sources_list <- list()
+  if ("SITE" %in% Formats){
+    SITE_sources <- DGVMBenchmarks::define_SITE_Sources(input = input)
+    all_SITE_datasets <- SITE_sources[[1]]
+    all_datasets[["SITE"]] <- all_SITE_datasets
+    all_SITE_simulation_Sources_list <- SITE_sources[[2]]
+    all_simulation_Sources_list[["SITE"]] <- all_SITE_simulation_Sources_list}
+  
+  if ("ICOS" %in% Formats){
+    all_ICOS_datasets <- DGVMBenchmarks::define_ICOS_DatasetSource(input = input)
+    all_datasets[["ICOS"]] <- all_ICOS_datasets
+  }
+  
+  if ("FLUXNET" %in% Formats){
+    all_FLUXNET_datasets <- DGVMBenchmarks::define_FLUXNET_DatasetSource(input = input)
+    all_datasets[["FLUXNET"]] <- all_FLUXNET_datasets
+  }
   return(sources <- list(all_datasets,all_simulation_Sources_list))
 }
