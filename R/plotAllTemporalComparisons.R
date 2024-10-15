0e4845ae8a8205feed05b2172c2959b8fe9bada5
Karl Piltz <Karl.Piltz@nateko.lu.se>
2024-10-15 14:55:21 +0200

    Updt FLUXNET to follow placeholder coord key

--- R/plotAllTemporalComparisons.R
+++ R/plotAllTemporalComparisons.R
@@ -22,8 +22,9 @@ plotAllTemporalComparisons <- function(Benchmark = this_benchmark, all_compariso
   }
   
   if (Benchmark@datasets[[1]]@source@format@id == "FLUXNET"){
-    grid.names <- unique(Benchmark@datasets[[1]]@spatial.extent$Name)
-    names(grid.names) <- paste0("(", unique(Benchmark@datasets[[1]]@spatial.extent$Lon), ",", unique(Benchmark@datasets[[1]]@spatial.extent$Lat), ")")
+    FLUXNET_grid <- read.table(file.path(system.file("extdata/FLUXNET/FLUXNET_grid_china.txt", package = "DGVMBenchmarks")))  
+    grid.names <- FLUXNET_grid$Name
+    names(grid.names) <- paste0("(", FLUXNET_grid$GUESS_Lon, ",", FLUXNET_grid$GUESS_Lat, ")")
     }
   
   
